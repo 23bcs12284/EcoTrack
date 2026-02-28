84d259309145fc08f5e2615bc7d281f30529c9c7
Prabhakar Kumar Jha <prabhakarkumarjha@Prabhakars-MacBook-Air.local>
2026-02-28 18:54:11 +0530

    Fix import consistency for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,46 +6,45 @@ import Header from "./components/Header";
 import ProtectedRoute from "./Routes/ProtectedRoutes";
 import { Routes, Route, BrowserRouter, Navigate } from "react-router-dom";
 
-
-
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header title="Dashboard" />
-        <main>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-            
-            <Route
-              path="/dashboard"
-              element={
-                <ProtectedRoute>
-                  <Dashboard />
-                </ProtectedRoute>
-              }
-            />
-            <Route
-              path="/log"
-              element={
-                <ProtectedRoute>
-                  <Log />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<Navigate to="/dashboard" replace />} />
-            <Route
-            path="/dashboard/water"             // New route for Water Tracker
+    <BrowserRouter>
+      <Header title="Dashboard" />
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/log"
+            element={
+              <ProtectedRoute>
+                <Log />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/dashboard/water"
             element={
               <ProtectedRoute>
                 <WaterTracker />
               </ProtectedRoute>
             }
           />
-          </Routes>
-        </main>
-      </BrowserRouter>
-    </>
+
+          <Route path="*" element={<Navigate to="/dashboard" replace />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
